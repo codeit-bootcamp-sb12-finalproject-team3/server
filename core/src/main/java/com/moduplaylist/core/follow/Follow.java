cba854d780db3cebd37eb9938b030255ce0fd6ab
taesung <taesung819@gmail.com>
2026-09-07 18:23:32 +0900

    feat: implement follow entity

--- core/src/main/java/com/moduplaylist/core/follow/Follow.java
+++ core/src/main/java/com/moduplaylist/core/follow/Follow.java
@@ -1,9 +1,45 @@
 package com.moduplaylist.core.follow;
 
-import com.moduplaylist.core.common.BaseEntity;
-import jakarta.persistence.Entity;
+import com.github.f4b6a3.uuid.UuidCreator;
+import com.moduplaylist.core.user.User;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.util.Objects;
+import java.util.UUID;
+
+@Getter
 @Entity
-public class Follow extends BaseEntity {
+@Table(name = "follows")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Follow{
+
+    @Id
+    @Column(name = "id", updatable = false, nullable = false)
+    private UUID id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "follower_id", nullable = false, updatable = false)
+    private User follower;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @JoinColumn(name = "follower_id", nullable = false, updatable = false)
+    private User followee;
+
+    public Follow(User follower, User followee) {
+        this.follower = Objects.requireNonNull(follower);
+        this.followee = Objects.requireNonNull(followee);
+    }
+
+    @PrePersist
+    protected void init() {
+        if (this.id == null) {
+            this.id = UuidCreator.getTimeOrderedEpoch();
+        }
+    }
 }
 
