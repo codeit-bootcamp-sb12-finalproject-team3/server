40ac630b55676d53f5a2fd4dc923495435811682
nocturne9095 <nocturne9095@gmail.com>
2026-09-08 14:49:19 +0900

    Chore: app-realtime 모듈 경계 검증용 ArchUnit 테스트 추가

--- app-realtime/src/test/java/com/moduplaylist/realtime/ModuleBoundaryTest.java
+++ app-realtime/src/test/java/com/moduplaylist/realtime/ModuleBoundaryTest.java
@@ -1,4 +1,24 @@
 package com.moduplaylist.realtime;
 
-public class ModuleBoundaryTest {
+import com.tngtech.archunit.core.domain.JavaClasses;
+import com.tngtech.archunit.core.importer.ClassFileImporter;
+import com.tngtech.archunit.lang.ArchRule;
+import org.junit.jupiter.api.Test;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.noClasses;
+
+class ModuleBoundaryTest {
+
+    @Test
+    void appRealtimeShouldNotDependOnAppApiOrAppBatch() {
+        JavaClasses importedClasses = new ClassFileImporter()
+                .importPackages("com.moduplaylist");
+
+        ArchRule rule = noClasses()
+                .that().resideInAPackage("com.moduplaylist.realtime..")
+                .should().dependOnClassesThat()
+                .resideInAnyPackage("com.moduplaylist.api..", "com.moduplaylist.batch..");
+
+        rule.check(importedClasses);
+    }
 }
