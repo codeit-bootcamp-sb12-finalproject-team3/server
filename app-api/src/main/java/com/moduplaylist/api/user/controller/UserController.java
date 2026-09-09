210e7f6aa81fa9d16b327bd7c6ce96c950098a2b
aHjinee <101584386+aHjinee@users.noreply.github.com>
2026-09-09 17:48:47 +0900

    UserController.java 병합 과정에서 누락된 import추가

--- app-api/src/main/java/com/moduplaylist/api/user/controller/UserController.java
+++ app-api/src/main/java/com/moduplaylist/api/user/controller/UserController.java
@@ -12,6 +12,10 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.moduplaylist.api.recommendation.dto.UserPreferenceResponse;
+import java.util.UUID;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @RestController
 @RequestMapping("/api/users")
