982366278858f68c205daa29e9a7e6fe75ed59ca
taesung <taesung819@gmail.com>
2026-09-09 17:35:31 +0900

    refactor: update imports

--- core/src/main/java/com/moduplaylist/core/user/repository/UserRepository.java
+++ core/src/main/java/com/moduplaylist/core/user/repository/UserRepository.java
@@ -1,10 +1,9 @@
 package com.moduplaylist.core.user.repository;
 
 import com.moduplaylist.core.user.entity.User;
+import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-import java.util.UUID;
 
 public interface UserRepository extends JpaRepository<User, UUID> {
 
