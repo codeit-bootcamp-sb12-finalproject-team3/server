0585a00144c69d0b62a10e99f12ee25dc73581e9
taesung <taesung819@gmail.com>
2026-09-09 15:14:12 +0900

    fix: correct dev profile configuration

--- core/src/main/java/com/moduplaylist/core/user/repository/UserRepository.java
+++ core/src/main/java/com/moduplaylist/core/user/repository/UserRepository.java
@@ -1,10 +1,11 @@
 package com.moduplaylist.core.user.repository;
 
 import com.moduplaylist.core.user.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 import java.util.UUID;
 
-public interface UserRepository {
-    Optional<User> findById(UUID id);
+public interface UserRepository extends JpaRepository<User, UUID> {
+
 }
