6d7812b9543fc6eca2facf05e5f33cec8a833d14
minzy108 <minzy108@gmail.com>
2026-09-10 13:24:04 +0900

    feat: Spring Security 사용자 인증 정보 연동

---
+++ app-api/src/main/java/com/moduplaylist/api/global/security/CustomUserDetails.java
@@ -0,0 +1,60 @@
+package com.moduplaylist.api.global.security;
+
+import com.moduplaylist.api.user.dto.UserResponse;
+import java.util.Collection;
+import java.util.List;
+import java.util.Objects;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Getter
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+
+  private final UserResponse userResponse;
+  private final String password;
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return List.of(
+        new SimpleGrantedAuthority("ROLE_" + userResponse.getRole().name())
+    );
+  }
+
+  @Override
+  public String getUsername() {
+    return userResponse.getEmail();
+  }
+
+  @Override
+  public boolean isAccountNonLocked() {
+    return !userResponse.isLocked();
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj) {
+      return true;
+    }
+
+    if (obj == null || getClass() != obj.getClass()) {
+      return false;
+    }
+
+    CustomUserDetails other = (CustomUserDetails) obj;
+
+    return Objects.equals(
+        userResponse.getId(),
+        other.userResponse.getId()
+    );
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hashCode(userResponse.getId());
+  }
+
+}
