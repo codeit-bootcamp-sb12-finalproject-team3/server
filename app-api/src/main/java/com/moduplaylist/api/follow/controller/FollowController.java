27bc1196777909c8acb4c8ffa125e0c61b188ba1
taesung <taesung819@gmail.com>
2026-09-10 10:51:44 +0900

    refactor: change Follow controller variable

--- app-api/src/main/java/com/moduplaylist/api/follow/controller/FollowController.java
+++ app-api/src/main/java/com/moduplaylist/api/follow/controller/FollowController.java
@@ -33,8 +33,8 @@ public class FollowController {
                 .body(followService.create(followerId, request.getFolloweeId()));
     }
 
-    @DeleteMapping("/{followId}")
-    public ResponseEntity<Void>  delete(@PathVariable UUID followId) {
+    @DeleteMapping("/{followeeId}") /// 스웨거 명세와 달라 추후 프론트 작업 필수!!!!
+    public ResponseEntity<Void>  delete(@PathVariable UUID followeeId) {
         // TODO Security 구현 후
         // @AuthenticationPrincipal CustomUserDetails userDetails 로 변경
         // UUID followerId = userDetails.getUserId();
@@ -42,7 +42,7 @@ public class FollowController {
         UUID followerId = UUID.fromString(
                 "01a07a54-7f70-7e0d-9a80-15abccab6c92");
 
-        followService.delete(followerId, followId);
+        followService.delete(followerId, followeeId);
         return ResponseEntity.noContent().build();
     }
 
