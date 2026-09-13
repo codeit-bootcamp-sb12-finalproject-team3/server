432258bcbf584ebc299689125b1c9eb553eb9d7b
minzy108 <minzy108@gmail.com>
2026-09-14 00:07:11 +0900

    feat: 플레이리스트 조회용 repository 메서드 추가

--- core/src/main/java/com/moduplaylist/core/playlist/repository/PlaylistContentRepository.java
+++ core/src/main/java/com/moduplaylist/core/playlist/repository/PlaylistContentRepository.java
@@ -1,9 +1,11 @@
 package com.moduplaylist.core.playlist.repository;
 
 import com.moduplaylist.core.playlist.entity.PlaylistContent;
+import java.util.List;
 import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaylistContentRepository extends JpaRepository<PlaylistContent, UUID> {
 
+  List<PlaylistContent> findAllByPlaylist_Id(UUID playlistId);
 }
--- core/src/main/java/com/moduplaylist/core/playlist/repository/PlaylistSubscriptionRepository.java
+++ core/src/main/java/com/moduplaylist/core/playlist/repository/PlaylistSubscriptionRepository.java
@@ -6,4 +6,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PlaylistSubscriptionRepository extends JpaRepository<PlaylistSubscription, UUID> {
 
+  long CountByPlaylist_Id(UUID playlistId);
+
+  // 현재 로그인 사용자의 플레이리스트 구독 여부
+  boolean existsByPlaylist_IdAndUser_Id(UUID playlistId, UUID userId);
+
 }
